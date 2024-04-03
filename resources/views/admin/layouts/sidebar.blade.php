71e9b0421e0457fd1671924f91c9eb1f97cdafea
tesseractT <ebere.benny97@gmail.com>
2024-04-03 02:51:23 +0100

    admin sidebar icons

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,147 +9,18 @@
 
     </form>
     <ul class="navbar-nav navbar-right">
-        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
-                class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
-            <div class="dropdown-menu dropdown-list dropdown-menu-right">
-                <div class="dropdown-header">Messages
-                    <div class="float-right">
-                        <a href="#">Mark All As Read</a>
-                    </div>
-                </div>
-                <div class="dropdown-list-content dropdown-list-message">
-                    <a href="#" class="dropdown-item dropdown-item-unread">
-                        <div class="dropdown-item-avatar">
-                            <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle">
-                            <div class="is-online"></div>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Kusnaedi</b>
-                            <p>Hello, Bro!</p>
-                            <div class="time">10 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item dropdown-item-unread">
-                        <div class="dropdown-item-avatar">
-                            <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle">
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Dedik Sugiharto</b>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-                            <div class="time">12 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item dropdown-item-unread">
-                        <div class="dropdown-item-avatar">
-                            <img alt="image" src="assets/img/avatar/avatar-3.png" class="rounded-circle">
-                            <div class="is-online"></div>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Agung Ardiansyah</b>
-                            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <div class="time">12 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <div class="dropdown-item-avatar">
-                            <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle">
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Ardian Rahardiansyah</b>
-                            <p>Duis aute irure dolor in reprehenderit in voluptate velit ess</p>
-                            <div class="time">16 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <div class="dropdown-item-avatar">
-                            <img alt="image" src="assets/img/avatar/avatar-5.png" class="rounded-circle">
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Alfa Zulkarnain</b>
-                            <p>Exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
-                            <div class="time">Yesterday</div>
-                        </div>
-                    </a>
-                </div>
-                <div class="dropdown-footer text-center">
-                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
-                </div>
-            </div>
-        </li>
-        <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
-                class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
-            <div class="dropdown-menu dropdown-list dropdown-menu-right">
-                <div class="dropdown-header">Notifications
-                    <div class="float-right">
-                        <a href="#">Mark All As Read</a>
-                    </div>
-                </div>
-                <div class="dropdown-list-content dropdown-list-icons">
-                    <a href="#" class="dropdown-item dropdown-item-unread">
-                        <div class="dropdown-item-icon bg-primary text-white">
-                            <i class="fas fa-code"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            Template update is available now!
-                            <div class="time text-primary">2 Min Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <div class="dropdown-item-icon bg-info text-white">
-                            <i class="far fa-user"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>You</b> and <b>Dedik Sugiharto</b> are now friends
-                            <div class="time">10 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <div class="dropdown-item-icon bg-success text-white">
-                            <i class="fas fa-check"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            <b>Kusnaedi</b> has moved task <b>Fix bug header</b> to <b>Done</b>
-                            <div class="time">12 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <div class="dropdown-item-icon bg-danger text-white">
-                            <i class="fas fa-exclamation-triangle"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            Low disk space. Let's clean it!
-                            <div class="time">17 Hours Ago</div>
-                        </div>
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <div class="dropdown-item-icon bg-info text-white">
-                            <i class="fas fa-bell"></i>
-                        </div>
-                        <div class="dropdown-item-desc">
-                            Welcome to Stisla template!
-                            <div class="time">Yesterday</div>
-                        </div>
-                    </a>
-                </div>
-                <div class="dropdown-footer text-center">
-                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
-                </div>
-            </div>
-        </li>
+
         <li class="dropdown"><a href="#" data-toggle="dropdown"
                 class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                <img alt="image" src="{{ asset(auth()->user()->avatar) }}" class="rounded-circle mr-1">
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-title">Logged in 5 min ago</div>
                 <a href="{{ route('admin.profile') }}" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
-                <a href="features-activities.html" class="dropdown-item has-icon">
-                    <i class="fas fa-bolt"></i> Activities
-                </a>
-                <a href="features-settings.html" class="dropdown-item has-icon">
+
+                <a href="{{ route('admin.settings.index') }}" class="dropdown-item has-icon">
                     <i class="fas fa-cog"></i> Settings
                 </a>
                 <div class="dropdown-divider"></div>
@@ -174,24 +45,25 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Stisla</a>
+            <a href="{{ route('admin.dashboard.index') }}">{{ config('settings.site_name') }}</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="{{ route('admin.dashboard.index') }}">{{ truncateText(config('settings.site_name'), 2) }}</a>
         </div>
         <ul class="sidebar-menu">
 
-            <li class="menu-header">Starter</li>
+            <li class="menu-header">MENU</li>
             <li class="{{ setSidebarActive(['admin.dashboard.index']) }}"><a class="nav-link"
-                    href="{{ route('admin.dashboard.index') }}"><i class="far fa-square"></i>
+                    href="{{ route('admin.dashboard.index') }}"><i class="fas fa-home"></i>
                     <span>Dashboard
                     </span></a></li>
 
             {{-- Sections --}}
             @can('section index')
-                <li class="dropdown {{ setSidebarActive(['admin.hero.index']) }}">
-                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Sections</span></a>
+                <li
+                    class="dropdown {{ setSidebarActive(['admin.hero.index', 'admin.our-features.index', 'admin.counter.index']) }}">
+                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                        <span>Sections</span></a>
 
                     <ul class="dropdown-menu">
                         <li class="{{ setSidebarActive(['admin.hero.index']) }}"><a class="nav-link"
@@ -208,9 +80,9 @@
             @canany(['listing index', ' pending listing', 'listing review ', 'listing claims'])
                 <li
                     class="dropdown
-            {{ setSidebarActive(['admin.category.*', 'admin.location.*', 'admin.amenity.*', 'admin.listing.*', 'admin.pending-listing.*']) }}">
-                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Listings</span></a>
+            {{ setSidebarActive(['admin.category.*', 'admin.location.*', 'admin.amenity.*', 'admin.listing.*', 'admin.pending-listing.*', 'admin.listing-review.*', 'admin.listing-claims.*']) }}">
+                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-list-alt"></i>
+                        <span>Listings</span></a>
 
                     <ul class="dropdown-menu">
                         @can('listing index')
@@ -245,14 +117,14 @@
             @endcanany
             {{-- PAckages and Payments --}}
             @can('package index')
-                <li class="dropdown {{ setSidebarActive(['admin.package.index']) }}">
+                <li class="dropdown {{ setSidebarActive(['admin.package.*', 'admin.payment-settings.*']) }}">
                     <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Manage Packages</span></a>
+                            class="fas fa-credit-card"></i></i> <span>Manage Packages</span></a>
                     <ul class="dropdown-menu">
-                        <li class="{{ setSidebarActive(['admin.package.*']) }}"><a class="nav-link"
+                        <li class="{{ setSidebarActive(['admin.package.index']) }}"><a class="nav-link"
                                 href="{{ route('admin.package.index') }}">Packages</a></li>
                         @can('payment settings index')
-                            <li class="{{ setSidebarActive(['admin.package.*']) }}"><a class="nav-link"
+                            <li class="{{ setSidebarActive(['admin.payment-settings.index']) }}"><a class="nav-link"
                                     href="{{ route('admin.payment-settings.index') }}">Payment Settings</a></li>
                         @endcan
                     </ul>
@@ -261,25 +133,29 @@
 
             {{-- Order --}}
             @can('order index')
-                <li><a class="nav-link" href="{{ route('admin.orders.index') }}"><i class="far fa-square"></i> <span>
+                <li class="{{ setSidebarActive(['admin.orders.index']) }}"><a class="nav-link"
+                        href="{{ route('admin.orders.index') }}"><i class="fas fa-cart-plus"></i><span>
                             Orders</span></a></li>
             @endcan
             {{-- Message --}}
             @can('message index')
-                <li><a class="nav-link" href="{{ route('admin.messages.index') }}"><i class="far fa-square"></i> <span>
+                <li class="{{ setSidebarActive(['admin.messages.index']) }}"><a class="nav-link"
+                        href="{{ route('admin.messages.index') }}"><i class="fas fa-comment-alt"></i><span>
                             Messages</span></a></li>
             @endcan
             {{-- Testimonial --}}
             @can('testimonial index')
-                <li><a class="nav-link" href="{{ route('admin.testimonial.index') }}"><i class="far fa-square"></i>
+                <li class="{{ setSidebarActive(['admin.testimonial.index']) }}"><a class="nav-link"
+                        href="{{ route('admin.testimonial.index') }}"><i class="fas fa-smile"></i></i>
                         <span>
                             Testimonials</span></a></li>
             @endcan
             {{-- Blog --}}
             @can('blog index')
-                <li class="dropdown {{ setSidebarActive(['admin.blog-category.index']) }}">
-                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Manage Blog</span></a>
+                <li
+                    class="dropdown {{ setSidebarActive(['admin.blog-category.*', 'admin.blog.*', 'admin.blog-comment.*']) }}">
+                    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-rss"></i>
+                        <span>Manage Blog</span></a>
 
                     <ul class="dropdown-menu">
                         <li class="{{ setSidebarActive(['admin.blog-category.*']) }}"><a class="nav-link"
@@ -294,9 +170,15 @@
 
             {{-- Pages --}}
             @canany(['about index', 'contact index', 'privacy policy index', 'terms and conditions index'])
-                <li class="dropdown {{ setSidebarActive(['admin.blog-category.index']) }}">
+                <li
+                    class="dropdown {{ setSidebarActive([
+                        'admin.about-us.*',
+                        'admin.contact-us.*',
+                        'admin.privacy-policy.*',
+                        'admin.terms-and-conditions.*',
+                    ]) }}">
                     <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Pages</span></a>
+                            class="far fa-file-alt"></i> <span>Pages</span></a>
 
                     <ul class="dropdown-menu">
                         @can('about index')
@@ -321,9 +203,9 @@
 
             {{-- Footer --}}
             @can('footer index')
-                <li class="dropdown {{ setSidebarActive(['admin.footer-info.index']) }}">
+                <li class="dropdown {{ setSidebarActive(['admin.footer-info.*', 'admin.social-link.*']) }}">
                     <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Manage Footer</span></a>
+                            class="fas fa-info-circle"></i> <span>Manage Footer</span></a>
 
                     <ul class="dropdown-menu">
                         <li class="{{ setSidebarActive(['admin.footer-info.*']) }}"><a class="nav-link"
@@ -336,12 +218,12 @@
 
             {{-- Access Management --}}
             @can('access management index')
-                <li class="dropdown {{ setSidebarActive(['admin.footer-info.index']) }}">
+                <li class="dropdown {{ setSidebarActive(['admin.role-user.*', 'admin.role.*']) }}">
                     <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
-                            class="fas fa-columns"></i> <span>Access Management</span></a>
+                            class="fas fa-fingerprint"></i> <span>Access Management</span></a>
 
                     <ul class="dropdown-menu">
-                        <li class="{{ setSidebarActive(['admin.role.*']) }}"><a class="nav-link"
+                        <li class="{{ setSidebarActive(['admin.role-user.*']) }}"><a class="nav-link"
                                 href="{{ route('admin.role-user.index') }}">Roles Users</a></li>
                         <li class="{{ setSidebarActive(['admin.role.*']) }}"><a class="nav-link"
                                 href="{{ route('admin.role.index') }}">Roles and Permissions</a></li>
@@ -352,20 +234,23 @@
 
             {{-- Menu Builder --}}
             @can('menu builder index')
-                <li><a class="nav-link" href="{{ route('admin.menu-builder.index') }}"><i class="far fa-square"></i>
+                <li class="{{ setSidebarActive(['admin.menu-builder.*']) }}"><a class="nav-link"
+                        href="{{ route('admin.menu-builder.index') }}"><i class="fas fa-wrench"></i>
                         <span>
                             Menu Builder</span></a></li>
             @endcan
 
             {{-- Settings --}}
             @can('settings index')
-                <li><a class="nav-link" href="{{ route('admin.settings.index') }}"><i class="far fa-square"></i> <span>
+                <li class="{{ setSidebarActive(['admin.settings.*']) }}"><a class="nav-link"
+                        href="{{ route('admin.settings.index') }}"><i class="fas fa-cogs"></i> <span>
                             Settings</span></a></li>
             @endcan
 
             {{-- Clear Database --}}
             @can('clear database')
-                <li><a class="nav-link" href="{{ route('admin.clear-database.index') }}"><i class="far fa-square"></i>
+                <li class="{{ setSidebarActive(['admin.clear-database.*']) }}"><a class="nav-link"
+                        href="{{ route('admin.clear-database.index') }}"><i class="fas fa-skull-crossbones"></i>
                         <span>
                             Wipe Database</span></a></li>
             @endcan
